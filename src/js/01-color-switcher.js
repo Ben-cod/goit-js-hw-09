ad45e16cafdcfae08e5bb83ffb0dbc4bd450a017
Ben-cod <123839625+Ben-cod@users.noreply.github.com>
2023-06-15 23:46:51 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,26 +1,49 @@
-const startButton = document.querySelector('[data-start]');
-const stopButton = document.querySelector('[data-stop]');
+// const startButton = document.querySelector('[data-start]');
+// const stopButton = document.querySelector('[data-stop]');
 
-let intervalId = null;
+// let intervalId = null;
 
-startButton.addEventListener('click', startColorChange);
-stopButton.addEventListener('click', stopColorChange);
+// startButton.addEventListener('click', startColorChange);
+// stopButton.addEventListener('click', stopColorChange);
 
-function startColorChange() {
-  startButton.disabled = true; // Вимикаємо кнопку "Start"
-  intervalId = setInterval(changeBackgroundColor, 1000);
-}
+// function startColorChange() {
+//   startButton.disabled = true; // Вимикаємо кнопку "Start"
+//   intervalId = setInterval(changeBackgroundColor, 1000);
+// };
 
-function stopColorChange() {
-  startButton.disabled = false; // Увімкнюємо кнопку "Start"
-  clearInterval(intervalId);
-}
+  
+//   function stopColorChange() {
+//     startButton.disabled = false; // Увімкнюємо кнопку "Start"
+//     clearInterval(intervalId);
+//   }
+//   function changeBackgroundColor() {
+//     const randomColor = getRandomHexColor();
+//     document.body.style.backgroundColor = randomColor;
+//   }
+ 
+  const botumOn = document.querySelector('data-start');
+  const botumOff = document.querySelector('data-stop');
+  let intervalId = null;
+  
+  botumOn.addEventListener(click, startColorChange);
+  botumOff.addEventListener(click, stopColorChange);
+  
 
-function changeBackgroundColor() {
-  const randomColor = getRandomHexColor();
-  document.body.style.backgroundColor = randomColor;
-}
+  function startColorChange(){
+    botumOn.disabled = true;
+    intervalId = setInterval(changeBackgroundColor, 1000)
+  };
 
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
-}
+  function stopColorChange(){
+    botumOn.disabled = false;
+    clearInterval(intervalId);
+  };
+
+  function changeBackgroundColor(){
+    const randomColor = getRandomHexColor();
+    document.body.style.background = randomColor;
+  };
+
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+  };
